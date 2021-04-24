dc509e17db3f92ac911729fe7c713c98db216baa
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-04-24 17:56:55 +0600

    upload

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello')
+class Pendrive {
+    constructor(name, color, price) {
+        this.name = name,
+        this.color = color,
+        this.price = price
+    }
+
+    getColor() {
+        return this.color
+    }
+
+    goForGrocery() {
+        if(this.getColor) {
+           this.getColor()
+        }
+        return 'i will buy'
+    }
+}
+
+const adidasPendrive = new Pendrive('adidas', 'red', 200)
+console.log(adidasPendrive.goForGrocery())
